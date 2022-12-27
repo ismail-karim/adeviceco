d404387747c972467f6921be39d6ad62aa6114ec
ismail-karim <ikarimallah@gmail.com>
2022-12-27 17:04:26 +0100

    Add firebase to env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,14 @@ export const environment = {
   production: true,
   preprod: false,
   dev: false,
-  name: 'PROD'
+  name: 'PROD',
+  firebase: {
+    apiKey: "AIzaSyDiQ-oaYvVdwvnVkgiidIGr4gm1kaPvRVs",
+    authDomain: "adeviceco.firebaseapp.com",
+    projectId: "adeviceco",
+    storageBucket: "adeviceco.appspot.com",
+    messagingSenderId: "991210204793",
+    appId: "1:991210204793:web:391b4eaf7bd4d0f074eea8",
+    measurementId: "G-LGG6994WRT"
+  }
 };
